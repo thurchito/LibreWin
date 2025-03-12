189c81b4b05fe692c0e5e94c34feaf3c835fa754
Kap Petrov <pathaksabita31@gmail.com>
2025-03-12 11:30:02 +0530

    build.sh: Add a comment for clarification

--- src/build.sh
+++ src/build.sh
@@ -1,3 +1,4 @@
-export PATH="/home/kap/Downloads/bin:$PATH"
+# Add the path of where your i686-elf-gcc cross compiler tools are.
+# export PATH="/home/kap/Downloads/bin:$PATH"
 make all
 qemu-system-i386 -drive format=raw,file=./bin/os.bin -serial stdio
