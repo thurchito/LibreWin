ce2211cc807014eabaca3baba8a9a872cca0c0d0
realblobii <realblobii@proton.me>
2025-03-29 12:36:57 +0800

    forogr to make it veta 3

--- free95/applications/buildno.c
+++ free95/applications/buildno.c
@@ -29,7 +29,7 @@ void RtlCreateStringFromUint(unsigned int value, char *str);
 void _start()
 {
     unsigned int nBuild = 5400;
-    unsigned int nBeta = 2;
+    unsigned int nBeta = 3;
     unsigned int nMajorVersion = 0;
     unsigned int nMiddleVersion = 3;
     unsigned int nMinorVersion = 0;
