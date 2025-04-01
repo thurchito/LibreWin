f08f593a76d81faae531057c8e91951cccdc6e88
sebscripts <sebzero227@gmail.com>
2025-04-01 23:31:08 +0800

    more friendly

--- free95/buildexe.sh
+++ free95/buildexe.sh
@@ -1,7 +1,7 @@
-i686-w64-mingw32-gcc -nostdlib -o open.exe applications/program.c applications/appinclude/print.c -Iapplications/appinclude
-i686-w64-mingw32-gcc -nostdlib -o hello.exe applications/hello.c applications/appinclude/print.c -Iapplications/appinclude
-i686-w64-mingw32-gcc -nostdlib -o freever.exe applications/buildno.c applications/appinclude/print.c -Iapplications/appinclude
-i686-w64-mingw32-gcc -nostdlib -o lsbin.exe applications/lsbin.c applications/appinclude/print.c -Iapplications/appinclude
+APPS=("open.exe" "hello.exe" "freever.exe" "lsbin.exe")
+for app in "${APPS[@]}"; do
+  i686-w64-mingw32-gcc -nostdlib -o "$app" "applications/${app%.exe}.c" applications/appinclude/print.c -Iapplications/appinclude
+done
+
 i686-w64-mingw32-gcc -nostdlib -o stop.exe applications/shutdown.c
 i686-w64-mingw32-gcc -nostdlib -o reboot.exe applications/reboot.c
-i686-w64-mingw32-gcc -nostdlib -o bsod.exe applications/bsod.c applications/appinclude/print.c -Iapplications/appinclude
