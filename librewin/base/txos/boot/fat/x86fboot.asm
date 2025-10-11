7ccf669585d2a8f5864c6c84063f43f8156e458d
Arthur Mendes <126976634+thurchito@users.noreply.github.com>
2025-10-10 23:22:23 -0300

    Bugfixes

--- librewin/base/txos/boot/fat/x86fboot.asm
+++ librewin/base/txos/boot/fat/x86fboot.asm
@@ -33,18 +33,22 @@ start:
     mov sp, 0x7c00
 
     ; Read kernel sectors in real mode
-    mov eax, 284      ; LBA of kernel start
-    mov ecx, 105      ; Number of sectors to read
+    mov eax, 284
+    mov ecx, 105
     mov edi, 0x00100000
     call ata_lba_read
 
-    ; Load GDT and switch to protected mode
     lgdt [gdt_descriptor]
+
+[BITS 32]
     mov eax, cr0
     or  eax, 1
     mov cr0, eax
-    jmp 0x08:protected_entry   ; 0x08 = code selector
 
+    ; Far jump to protected_entry (CS selector 0x08)
+    jmp 0x08:protected_entry
+
+[BITS 16]
 gdt_start:
 gdt_null:    dd 0,0
 
@@ -68,46 +72,55 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
+[BITS 32]
 protected_entry:
-    mov ax, 0x10      ; data selector
+    ; set up flat data segments and stack
+    mov ax, 0x10
     mov ds, ax
     mov es, ax
     mov ss, ax
-    mov esp, 0x90000  ; stack
+    mov esp, 0x90000
 
     ; Jump to kernel (already loaded at 0x00100000)
     jmp 0x08:0x00100000
 
+[BITS 16]
 ata_lba_read:
+    push bx
+    push si
+
+    mov bx, ax
+
+[BITS 32]
     push ebx
     push esi
 
-    mov ebx, eax       ; save LBA
+    mov ebx, eax       ; full LBA
     mov esi, ecx       ; sectors remaining
 
 .next_sector:
-    ; send sector count
-    mov dx, 0x1F2
-    mov al, sil        ; low byte of sectors (assumes <256)
-    out dx, al
+    ; sector count
+    mov al, sil
+    out 0x1F2, al
 
-    ; send LBA bytes
-    mov eax, ebx
-    mov dx, 0x1F3
-    out dx, al          ; LBA 0..7
-    mov al, ah
-    inc dx
-    out dx, al          ; LBA 8..15
-    shr ebx, 16
+    ; LBA low
     mov al, bl
-    inc dx
-    out dx, al          ; LBA 16..23
-    mov al, bh
-    or al, 0xE0
-    inc dx
-    out dx, al          ; LBA 24..27 + master
+    out 0x1F3, al
 
-    ; send READ SECTORS command
+    ; LBA mid
+    mov al, bh
+    out 0x1F4, al
+
+    ; LBA high
+    mov al, byte [ebx + 2]
+    out 0x1F5, al
+
+    ; top + head + drive
+    mov al, byte [ebx + 3]
+    or al, 0xE0
+    out 0x1F6, al
+
+    ; READ SECTORS
     mov al, 0x20
     out 0x1F7, al
 
@@ -116,7 +129,6 @@ ata_lba_read:
     test al, 8
     jz .wait_drq
 
-    ; read 256 words (512 bytes)
     mov dx, 0x1F0
     mov ecx, 256
     rep insw
@@ -127,6 +139,10 @@ ata_lba_read:
 
     pop esi
     pop ebx
+
+[BITS 16]
+    pop si
+    pop bx
     ret
 
 times 510-($-$$) db 0
